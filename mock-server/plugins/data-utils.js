00f51eb8fd6edf1d49082b14136bd525809ae7bd
Sigrid Keydana <469371+skeydan@users.noreply.github.com>
2024-09-04 12:46:51 +0200

    LFW111 (wip)

---
+++ mock-server/plugins/data-utils.js
@@ -0,0 +1,56 @@
+"use strict";
+import fp from "fastify-plugin";
+import { PassThrough } from "node:stream";
+
+// Mock data
+const orders = {
+  A1: { total: 3 },
+  A2: { total: 7 },
+  B1: { total: 101 },
+};
+
+// Map category to ID prefix
+const catToPrefix = {
+  electronics: "A",
+  confectionery: "B",
+}; 
+// Simulate realtime orders
+ // Create a stream of orders
+ const orderStream = new PassThrough({ objectMode: true });
+
+ // Simulate real-time orders
+ async function* realtimeOrdersSimulator() {
+   for await (const { id, total } of orderStream) {
+     yield JSON.stringify({ id, total });
+   }
+ } 
+
+// Return current orders
+function* currentOrders(category) {
+  console.log("in currentOrders");
+  const idPrefix = catToPrefix[category];
+  if (!idPrefix) return;
+  const ids = Object.keys(orders).filter((id) => id[0] === idPrefix);
+  for (const id of ids) {
+    yield JSON.stringify({ id, ...orders[id] });
+  }
+}
+
+// Calculate next ID
+const calculateID = (idPrefix, data) => {
+  const sorted = [...new Set(data.map(({ id }) => id))];
+  const next = Number(sorted.pop().slice(1)) + 1;
+  return `${idPrefix}${next}`;
+};
+
+// Plugin
+export default fp(async function (fastify, opts) {
+  fastify.decorate("currentOrders", currentOrders);
+  fastify.decorate("realtimeOrders", realtimeOrdersSimulator);
+  fastify.decorate("mockDataInsert", function (request, category, data) {
+    const idPrefix = catToPrefix[category];
+    const id = calculateID(idPrefix, data);
+    data.push({ id, ...request.body });
+    return data;
+  });
+}); 
